aef3140823fe0cc862fff3998f2f501cddecc405
Rob Waller <r.t.waller@gmail.com>
2018-01-24 13:25:42 -0600

    UI finished for creating songs. Stopped at 49min

--- client/src/components/CreateSong.vue
+++ client/src/components/CreateSong.vue
@@ -4,32 +4,32 @@
             <panel title="Song Metadata">
                 <v-text-field
                     label="Title"
-                    v-model="title"
+                    v-model="song.title"
                 ></v-text-field>
 
                 <v-text-field
                     label="Artist"
-                    v-model="artist"
+                    v-model="song.artist"
                 ></v-text-field>
 
                 <v-text-field
                     label="Genre"
-                    v-model="genre"
+                    v-model="song.genre"
                 ></v-text-field>
 
                 <v-text-field
                     label="Album"
-                    v-model="album"
+                    v-model="song.album"
                 ></v-text-field>
 
                 <v-text-field
                     label="Album Image Url"
-                    v-model="albumImageUrl"
+                    v-model="song.albumImageUrl"
                 ></v-text-field>
 
                 <v-text-field
                     label="Youtube ID"
-                    v-model="youtubeID"
+                    v-model="song.youtubeID"
                 ></v-text-field>
             </panel>
         </v-flex>
@@ -59,22 +59,32 @@
 
 <script>
 import Panel from '@/components/Panel'
+import SongsService from '@/services/SongsService'
 export default {
     data () {
         return {
-            title: null,
-            artist: null,
-            genre: null,
-            album: null,
-            albumImageUrl: null,
-            youtubeID: null,
-            lyrics: null,
-            tab: null
+            song: {
+                title: null,
+                artist: null,
+                genre: null,
+                album: null,
+                albumImageUrl: null,
+                youtubeID: null,
+                lyrics: null,
+                tab: null
+            }
         }
     },
     methods : {
-        create () {
-            //  call api
+        async create () {
+            try {
+                await SongsService.post(this.song)
+                this.$router.push({
+                    name: "songs"
+                })
+            } catch (err) {
+                console.log(err)
+            }
         }
     },
     components: {
