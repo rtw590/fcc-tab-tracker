b83f3960ed3c384b46d96baf93c56b875161e57f
Rob Waller <r.t.waller@gmail.com>
2018-01-26 12:25:15 -0600

    styled browse page and added view button for individual songs

--- client/src/components/Songs.vue
+++ client/src/components/Songs.vue
@@ -16,10 +16,38 @@
         </v-btn>
         <div 
           v-for="song in songs" 
+          class="song"
           :key="song.id">
-          {{song.title}} -
-          {{song.artist}} -
-          {{song.album}}
+
+          <v-layout>
+            <v-flex xs6>
+              <div class="song-title">
+                {{song.title}}
+              </div>
+              <div class="song-artist">
+                {{song.artist}}
+              </div>
+              <div class="song-genre">
+                {{song.genre}}
+              </div>
+
+              <v-btn
+                dark
+                class="cyan"
+                @click="navigateTo({
+                  name: 'song', 
+                  params: {
+                    songId: song.id
+                  }
+                })">
+                View
+              </v-btn>
+            </v-flex>
+              
+            <v-flex xs6>
+              <img :src="song.albumImageUrl" alt="" class="album-image" />
+            </v-flex>
+          </v-layout>
         </div>
 
       </panel>
@@ -53,5 +81,28 @@ export default {
 </script>
 
 <style scoped>
+
+.song {
+  padding: 20px;
+  height: 330px;
+  overflow: hidden;
+}
+
+.song-title {
+  font-size: 30px
+}
+
+.song-artist {
+  font-size: 24px
+}
+
+.song-genre {
+  font-size: 18px
+}
+
+.album-image {
+  width: 70%;
+  margin: 0 auto;
+}
 
 </style>
