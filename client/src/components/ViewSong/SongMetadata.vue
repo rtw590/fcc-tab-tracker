4f8fec6be0348f4fb1d5e53d56683a5614ed6d74
Rob Waller <r.t.waller@gmail.com>
2018-01-26 13:47:48 -0600

    in the middle of reworking ViewSong by adding SongMetadata subcomponent. Stopped at 22:20

---
+++ client/src/components/ViewSong/SongMetadata.vue
@@ -0,0 +1,32 @@
+<template>
+    <panel title="Song Metadata">
+        <v-layout>
+            <v-flex xs6>
+                <div class="song-title">
+                    {{song.title}}
+                </div>
+                <div class="song-artist">
+                    {{song.artist}}
+                </div>
+                <div class="song-genre">
+                    {{song.genre}}
+                </div>
+            </v-flex>
+            
+            <v-flex xs6>
+                <img :src="song.albumImageUrl" alt="" class="album-image" />
+                <br>
+                {{song.album}}
+            </v-flex>
+        </v-layout>
+    </panel>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ client/src/components/ViewSong/ViewSong.vue
@@ -0,0 +1,114 @@
+<template>
+    <div>
+        <v-layout>
+            <v-flex xs6>
+                <panel title="Song Metadata">
+                <v-layout>
+                    <v-flex xs6>
+                        <div class="song-title">
+                            {{song.title}}
+                        </div>
+                        <div class="song-artist">
+                            {{song.artist}}
+                        </div>
+                        <div class="song-genre">
+                            {{song.genre}}
+                        </div>
+            </v-flex>
+                
+                <v-flex xs6>
+                <img :src="song.albumImageUrl" alt="" class="album-image" />
+                <br>
+                {{song.album}}
+                </v-flex>
+            </v-layout>
+                </panel>
+            </v-flex>
+            <v-flex xs6>
+                <panel title="Tabs" class="ml-2">
+                    <textarea
+                        readonly
+                        v-model="song.tab"
+                    ></textarea>
+                </panel>
+            </v-flex>
+        </v-layout>
+        
+        <v-layout>
+            <v-flex xs6>
+                <panel title="Youtube Video">
+                
+                </panel>
+            </v-flex>
+            </v-layout>
+
+        <v-layout>
+            <v-flex xs6>
+                <panel title="Lyrics" class="ml-2">
+                    <textarea
+                        readonly
+                        v-model="song.lyrics"
+                    ></textarea>
+                </panel>
+            </v-flex>
+        </v-layout>
+    </div>
+</template>
+
+<script>
+import SongsService from '@/services/SongsService'
+import Panel from '@/components/Panel'
+
+export default {
+    data () {
+        return {
+            song: {}
+        }
+    },
+    async mounted () {
+        const songId = this.$store.state.route.params.songId
+        this.song = (await SongsService.show(songId)).data
+    },
+    components: {
+        Panel
+    }
+}
+</script>
+
+<style scoped>
+
+.song {
+  padding: 20px;
+  height: 330px;
+  overflow: hidden;
+}
+
+.song-title {
+  font-size: 30px
+}
+
+.song-artist {
+  font-size: 24px
+}
+
+.song-genre {
+  font-size: 18px
+}
+
+.album-image {
+  width: 70%;
+  margin: 0 auto;
+}
+
+textarea {
+    width: 100%;
+    font-family: monospace;
+    border: none;
+    height: 600px;
+    border-style: none;
+    border-color: transparent;
+    overflow: auto;
+    padding: 40px;
+}
+
+</style>
